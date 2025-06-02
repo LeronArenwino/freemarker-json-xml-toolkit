bd22d06615fadf21d6a46b33fade769c9ce1e2c2
Francisco Dueñas <Sebasduca@gmail.com>
2025-06-02 13:58:15 -0500

    feat: Add TemplateEditor class

---
+++ infrastructure/entry-points/gui/src/main/java/co/com/leronarenwino/editor/TemplateEditor.java
@@ -0,0 +1,21 @@
+/*
+ * This file is part of FreeMarker Tool.
+ *
+ * FreeMarker Tool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeMarker Tool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with FreeMarker Tool. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.com.leronarenwino.editor;
+
+public class TemplateEditor {
+}
