b2a672abec5d29d324f3efde6bf4262e82ab726e
Francisco Dueñas <Sebasduca@gmail.com>
2025-06-03 00:15:23 -0500

    refactor: Update project name from FreeMarker Tool to Template Tool

--- domain/usecase/src/main/java/co/com/leronarenwino/TemplateValidator.java
+++ domain/usecase/src/main/java/co/com/leronarenwino/TemplateValidator.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of FreeMarker Tool.
+ * This file is part of Template Tool.
  *
- * FreeMarker Tool is free software: you can redistribute it and/or modify
+ * Template Tool is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * FreeMarker Tool is distributed in the hope that it will be useful,
+ * Template Tool is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with FreeMarker Tool. If not, see <https://www.gnu.org/licenses/>.
+ * along with Template Tool. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package co.com.leronarenwino;
--- infrastructure/entry-points/gui/src/main/java/co/com/leronarenwino/editor/TemplateEditor.java
+++ infrastructure/entry-points/gui/src/main/java/co/com/leronarenwino/editor/TemplateEditor.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of FreeMarker Tool.
+ * This file is part of Template Tool.
  *
- * FreeMarker Tool is free software: you can redistribute it and/or modify
+ * Template Tool is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * FreeMarker Tool is distributed in the hope that it will be useful,
+ * Template Tool is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with FreeMarker Tool. If not, see <https://www.gnu.org/licenses/>.
+ * along with Template Tool. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package co.com.leronarenwino.editor;
@@ -79,7 +79,7 @@ public class TemplateEditor extends JFrame {
     public void initComponents() {
 
         // Set default configuration to JFrame
-        setTitle("FreeMarker Tool");
+        setTitle("Template Tool");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
