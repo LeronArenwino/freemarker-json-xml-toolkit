ccde7524161f50dfedb53dcba9eb4bc5591523fb
Francisco Dueñas <Sebasduca@gmail.com>
2025-06-04 09:05:50 -0500

    refactor: Clean up TemplateEditor and TemplateValidator by removing unused imports and simplifying JSON output generation

--- domain/usecase/src/main/java/co/com/leronarenwino/TemplateValidator.java
+++ domain/usecase/src/main/java/co/com/leronarenwino/TemplateValidator.java
@@ -24,7 +24,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
